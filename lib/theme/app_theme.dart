4ee441e99ed0df1d9ce09567f89adab37c93a071
Wandile Kubheka <retroff01@gmail.com>
2025-09-06 11:32:55 +0200

    added google fonts

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,1 +1,43 @@
-class AppTheme {}
+import 'package:flutter/material.dart';
+import "package:open_fashion/theme/color_theme.dart";
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class AppTheme {
+  static final ThemeData appThemeData = ThemeData(
+    textTheme: TextTheme(
+      bodyLarge: TextStyle(
+        fontSize: 16.0,
+        fontWeight: FontWeight.w400,
+        color: ColorTheme.bodyColor,
+        fontFamily: GoogleFonts.tenorSans().fontFamily,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 14.0,
+        fontWeight: FontWeight.w400,
+        color: ColorTheme.bodyColor,
+        fontFamily: GoogleFonts.tenorSans().fontFamily,
+      ),
+      bodySmall: TextStyle(
+        fontSize: 12.0,
+        fontWeight: FontWeight.w400,
+        color: ColorTheme.bodyColor,
+        fontFamily: GoogleFonts.tenorSans().fontFamily,
+      ),
+      titleLarge: TextStyle(
+        fontSize: 18.0,
+        color: ColorTheme.titleActive,
+        fontFamily: GoogleFonts.tenorSans().fontFamily,
+      ),
+      titleMedium: TextStyle(
+        fontSize: 16.0,
+        color: ColorTheme.titleActive,
+        fontFamily: GoogleFonts.tenorSans().fontFamily,
+      ),
+      titleSmall: TextStyle(
+        fontSize: 14.0,
+        color: ColorTheme.titleActive,
+        fontFamily: GoogleFonts.tenorSans().fontFamily,
+      ),
+    ),
+  );
+}
--- lib/theme/color_theme.dart
+++ lib/theme/color_theme.dart
@@ -1,1 +1,15 @@
-class ColorTheme {}
+import 'package:flutter/material.dart';
+
+abstract class ColorTheme {
+  // Define color palette here
+  static final Color primary = Color(0xFFA8715A);
+  static final Color secondary = Color(0xFFDD8560);
+  static final Color background = Color(0xFFF8F0E7);
+  static final Color labelColor = Color(0xFF555555);
+  static final Color placeholder = Color(0xFF888888);
+  static final Color bodyColor = Color(0xFF333333);
+  static final Color titleActive = Color(0xFF000000);
+  static final Color offWhite = Color(0xFFFCFCFC);
+  static final lineColor = Color(0xFFE0CFBA);
+  static final inputBackground = Color(0xFFF9F9F9);
+}
